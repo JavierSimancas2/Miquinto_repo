b74061931ef6557a61b02f8464beaa8bd2517465
Javier_Simancas <Javier.Simancas.2005@gmail.com>
2025-06-13 22:28:46 -0400

    Con el primer lanzamiento para este nuevo paquete

---
+++ build/lib/Miquinto_repo/__init__.py
@@ -0,0 +1,2 @@
+def saludar(Javier):
+    return "Hola " + nombre + ", este es mi primer paquete pip!"
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Miquinto_repo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Javier Simancas",                         # Tu nombre
+    author_email="Javier.Simancas.2005@gmail",                 # Tu correo electrónico
+    url="https://github.com/platzi/git-github",     # URL del proyecto
+)
